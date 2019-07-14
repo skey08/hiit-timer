be9e4faff2429572abbc608204a2295c447e116a
Ly Nguyen <Ly900@aol.com>
2019-07-13 22:20:46 -0400

    add flex to header to align items

--- app.js
+++ app.js
@@ -1,24 +1,15 @@
-
-const Logo = () => {
-	return (
-		<div class="timer__logo">
-			<h1 className="timer__main-heading sr-only">HIIT Timer</h1>
-			<img class="timer__logo-img" src="./assets/logo.png"/>
-		</div>
-	)
-}
-
 const Header = () => {
 	return (
 		<header class="timer__header">
-			<Logo />
+			<h1 className="timer__main-heading sr-only">HIIT Timer</h1>
+			<img class="timer__logo-img" src="./assets/logo.png"/>
 		</header>
 	)
 }
 
 const TimerPrompt = () => {
 	return (
-		<div className="timer__prompt">
+		<div className="timer__body">
 			<button className="timer__btn">Simple HIIT Timer</button>
 			<button className="timer__btn">Custom Timer with Exercises</button>
 		</div>
