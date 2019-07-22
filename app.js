ef2a0f0174853c2551c8cab87011dc181b9fabea
Ly Nguyen <Ly900@aol.com>
2019-07-22 14:15:18 -0400

    return different headers based on screen num

--- app.js
+++ app.js
@@ -1,20 +1,59 @@
-const Header = () => {
+
+
+const Header = (props) => {
+
+	console.log(props);
+	console.log("props.currentScreen: ", props.currentScreen);
+	let screenToReturn = "hey there";
+
+	const screen1 = {
+		icon: "",
+		title: "Screen1 Title"
+	}
+
+	const screen2 = {
+		icon: "",
+		title: "Screen2 Title"
+	}
+
+	switch(props.currentScreen) {
+		case 0:
+			screenToReturn = <img className="timer__logo-img" src="./assets/logo.png"/>;
+			break;
+		case 1:
+			screenToReturn = screen2.title;
+			break;
+		default:
+			screenToReturn = "";
+	}
+
+	// let headerToShow = 
+
+	// console.log("screen1: ", screen1);
+
+	// let headerInfo = props.screenInfo(props.currentScreen);
+	// console.log("headerInfo from Header");
+	// console.log(headerInfo); 
+	// console.log(headerInfo.title);
+
 	return (
-		<header className="timer__header">
+		<header className={`timer__header ${props}`}>
 			<h1 className="timer__main-heading sr-only">HIIT Timer</h1>
-			<img className="timer__logo-img" src="./assets/logo.png"/>
+			{/* <img className="timer__logo-img" src="./assets/logo.png"/> */}
+			{ screenToReturn }
 		</header>
 	)
 }
 
 const TimerPrompt = (props) => {
+	// console.log(props.currentScreen);
 	return (
 		<div className="timer__body">
 			<button className="timer__btn">
 				<picture className="timer__btn-picture">
 					<img src="./assets/icon-ring-simple.png" className="timer__btn-icon" />
 				</picture>
-				<span className="timer__btn-text" onClick={ () => props.nextScreen() }>Simple</span>
+				<span className="timer__btn-text" onClick={ () => props.nextScreen(props.currentScreen) }>Simple</span>
 			</button>
 			<button className="timer__btn">
 				<picture className="timer__btn-picture">
@@ -35,15 +74,45 @@ class Timer extends React.Component {
 		}
 	}
 
-	nextScreen() {
-		console.log("go to the next screen");
+	// getScreenInfo = (currentScreen) => {
+	// 	console.log("currentScreen from getScreenInfo: ", currentScreen);
+	// 	let screenToReturn = "";
+
+	// 	const screen1 = {
+	// 		icon: "",
+	// 		title: "Screen1 Title"
+	// 	}
+
+	// 	const screen2 = {
+	// 		icon: "",
+	// 		title: "Screen2 Title"
+	// 	}
+
+	// 	switch (currentScreen) {
+	// 		case 1: 
+	// 			screenToReturn = screen2;
+	// 			break;
+	// 	}
+
+	// 	return screenToReturn;
+
+	// }
+
+	nextScreen = (screenNum) => {
+		console.log("screenNum from nextScreen() before ++: ", screenNum);
+		// console.log("go to the next screen");
+		screenNum++;
+		console.log("screenNum from nextScreen() after ++: ", screenNum);
+		this.setState({
+			screen: screenNum
+		})
 	}
 
 	render() {
 		return(
 			<div className="timer__container">
-				<Header />
-				<TimerPrompt nextScreen={this.nextScreen}/>
+				<Header currentScreen={this.state.screen} />
+				<TimerPrompt nextScreen={this.nextScreen} currentScreen={this.state.screen}/>
 			</div>
 		)
 	}
