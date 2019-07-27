6b65bda687fbd71b526b117c153d809ea0eb89d4
Ly Nguyen <Ly900@aol.com>
2019-07-27 18:35:10 -0400

    create object literal to begin to set up body button info

--- app.js
+++ app.js
@@ -38,7 +38,64 @@ const Header = (props) => {
 
 const TimerPrompt = (props) => {
 	// console.log(props.currentScreen);
+	let iconToShow, titleToShow;
 	console.log(props);
+
+	let bodyContentInfo = [
+			{
+				screenNum: 0,
+				numOfButtons: 2,
+				buttonInfo: [
+					{
+						icon: "./assets/icon-ring-simple.png",
+						text: "Simple",
+						statePropToChange: "timerType",
+						changeTo: "simple"
+					},
+					{
+						icon: "./assets/icon-ring-custom.png",
+						text: "Complex",
+						statePropToChange: "timerType",
+						changeTo: "complex"
+					}
+				]
+			},
+			{
+				screenNum: 1,
+				numOfButtons: 4,
+				statePropToChange: "timeOn",
+				buttonInfo: [
+					{
+						icon: "./assets/icon-ring-simple.png",
+						text: "0:20",
+						changeTo: 20
+					},
+					{
+						icon: "./assets/icon-ring-custom.png",
+						text: "0:30",
+						changeTo: 30
+					}
+				]
+			}
+	]
+
+	switch(props.currentScreen) {
+		case 0:
+			iconToShow = <img className="timer__logo-img" src="./assets/logo.png"/>;
+			titleToShow = <h2 class="timer__screen-title sr-only">Home Screen</h2>;
+			break;
+		case 1:
+			iconToShow = <img className="timer__prompt-icon" src="./assets/icon-red-heart-green-bg.png"/>;
+			titleToShow = <h2 class="timer__screen-title">Exercise Time</h2>;
+			break;
+		case 2:
+			iconToShow = null;
+			titleToShow = <h2 class="timer__screen-title">Rest Time</h2>;
+			break;
+		default:
+			iconToShow = null;
+			titleToShow = "";
+	}
 
 	return (
 		<div className="timer__body">
@@ -67,6 +124,10 @@ class Timer extends React.Component {
 		}
 	}
 
+	getBodyContentInfo = (screenNum) => {
+		console.log(screenNum);
+	}
+
 	nextScreen = (screenNum) => {
 		// console.log("screenNum from nextScreen() before ++: ", screenNum);
 		// console.log("go to the next screen");
@@ -81,7 +142,7 @@ class Timer extends React.Component {
 		return(
 			<div className="timer__container">
 				<Header currentScreen={this.state.screen} />
-				<TimerPrompt nextScreen={this.nextScreen} currentScreen={this.state.screen}/>
+				<TimerPrompt nextScreen={this.nextScreen} currentScreen={this.state.screen} getBodyContentInfo={this.getBodyContentInfo} />
 			</div>
 		)
 	}
