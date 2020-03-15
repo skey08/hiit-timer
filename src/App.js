45f3666d214cd1f3eda824dd4841628ff432d62b
Ly Nguyen <Ly900@aol.com>
2020-03-15 17:04:37 -0400

    remove old comments

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import './App.css';
-
 import logo from './assets/logo.png';
-import './assets/icon-ring-simple.png';
-import './assets/icon-ring-custom.png';
 import redHeart from './assets/icon-red-heart-green-bg.png';
-// import bodyContentInfo from './body-content-info.json';
 
 const Header = (props) => {
-
-	// console.log(props);
-	// console.log("props.currentScreen: ", props.currentScreen);
-	// let screenToReturn = "hey there";
 	let iconToShow, titleToShow;
 
 	switch (props.currentScreen) {
@@ -75,137 +67,23 @@ const Button = (props) => {
 }
 
 const TimerPrompt = (props) => {
-	// console.log(props.currentScreen);
-	// let iconToShow, titleToShow;
-	// console.log("TimerPrompt props: ", props);
-	// console.log("current screen: ", props.currentScreen);
-
-	// const bodyContentInfo = [
-	// 	{
-	// 		screenNum: 0,
-	// 		hasIcons: true,
-	// 		buttonInfo: [
-	// 			{
-	// 				icon: ringSimple,
-	// 				text: "Simple",
-	// 				statePropToChange: "timerType",
-	// 				changeTo: "simple"
-	// 			},
-	// 			{
-	// 				icon: ringCustom,
-	// 				text: "Complex",
-	// 				statePropToChange: "timerType",
-	// 				changeTo: "complex"
-	// 			}
-	// 		]
-	// 	},
-	// 	{
-	// 		screenNum: 1,
-	// 		statePropToChange: "timeOn",
-	// 		hasIcons: false,
-	// 		buttonInfo: [
-	// 			{
-	// 				text: "0:20",
-	// 				changeTo: 20
-	// 			},
-	// 			{
-	// 				text: "0:30",
-	// 				changeTo: 30
-	// 			},
-	// 			{
-	// 				text: "0:45",
-	// 				changeTo: 45
-	// 			},
-	// 			{
-	// 				text: "1:00",
-	// 				changeTo: 60
-	// 			}
-	// 		]
-	// 	},
-	// 	{
-	// 		screenNum: 2,
-	// 		statePropToChange: "timeOff",
-	// 		hasIcons: false,
-	// 		buttonInfo: [
-	// 			{
-	// 				text: "0:10",
-	// 				changeTo: 10
-	// 			},
-	// 			{
-	// 				text: "0:30",
-	// 				changeTo: 30
-	// 			},
-	// 			{
-	// 				text: "1:00",
-	// 				changeTo: 60
-	// 			},
-	// 			{
-	// 				text: "2:00",
-	// 				changeTo: 120
-	// 			}
-	// 		]
-	// 	},
-	// 	{
-	// 		screenNum: 3,
-	// 		statePropToChange: "numOfSets",
-	// 		hasIcons: false,
-	// 		buttonInfo: [
-	// 			{
-	// 				text: "5",
-	// 				changeTo: 5
-	// 			},
-	// 			{
-	// 				text: "8",
-	// 				changeTo: 8
-	// 			},
-	// 			{
-	// 				text: "10",
-	// 				changeTo: 10
-	// 			},
-	// 			{
-	// 				text: "16",
-	// 				changeTo: 16
-	// 			}
-	// 		]
-	// 	}
-	// ]
 
 	if (props.loading) {
 		return <div>Loading...</div>;
 	}
 
-	const bodyContentInfo = props.bodyContentInfo;
-	// console.log(typeof (bodyContentInfo));
+	const screenToDisplay = props.bodyContentInfo.filter((item) => item.screenNum === props.currentScreen);
 
-	let screenToDisplay = bodyContentInfo.filter((item) => item.screenNum === props.currentScreen);
-
-	// console.log("screenToDisplay: ", screenToDisplay);
-
-	let buttonsToDisplay = screenToDisplay.map((button) => {
+	const buttonsToDisplay = screenToDisplay.map((button) => {
 		return button.buttonInfo.map((btn, index) => {
 			return (
-				<Button key={index} btnInfo={btn} bodyContentInfo={bodyContentInfo} handleClick={props.handleClick} />
+				<Button key={index} btnInfo={btn} bodyContentInfo={props.bodyContentInfo} handleClick={props.handleClick} />
 			)
 		})
 	})
 
-	// console.log("buttonsToDisplay: ", buttonsToDisplay);
-
 	return (
 		<div className={`timer__body timer__body_screen_${props.currentScreen}`}>
-			{/* <button className="timer__btn" onClick={ () => props.nextScreen(props.currentScreen) }>
-				<picture className="timer__btn-picture">
-					<img src="./assets/icon-ring-simple.png" className="timer__btn-icon" />
-				</picture>
-				<span className="timer__btn-text">Simple</span>
-			</button>
-			<button className="timer__btn">
-				<picture className="timer__btn-picture">
-					<img src="./assets/icon-ring-custom.png" className="timer__btn-icon" />
-				</picture>
-				<span className="timer__btn-text">Custom</span>
-			</button> */}
-			{/* <Button /> */}
 			{buttonsToDisplay}
 		</div>
 	)
